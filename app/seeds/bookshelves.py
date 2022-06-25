47a725644862817dbf26297fc2752ca9a4f62ba1
Andrew Stilinovic <stili87@gmail.com>
2022-06-25 13:04:55 -0500

    figured out many to many

--- app/seeds/bookshelves.py
+++ app/seeds/bookshelves.py
@@ -4,12 +4,12 @@ from app.models import db, BookShelf, Book
 
 def seed_bookshelves():
   books = Book.query.all() 
-  
+
 
   bookshelf1 = BookShelf(name="Yang", user_id= 1, bookshelves_book=books)
-  bookshelf2 = BookShelf(name="Eric ", user_id= 2)
-  bookshelf3 = BookShelf(name="Stili ", user_id= 3)
-  bookshelf4 = BookShelf(name="Dennis ", user_id= 2)
+  bookshelf2 = BookShelf(name="Eric ", user_id= 2, bookshelves_book=books)
+  bookshelf3 = BookShelf(name="Stili ", user_id= 3, bookshelves_book=books)
+  bookshelf4 = BookShelf(name="Dennis ", user_id= 2, bookshelves_book=books)
 
   db.session.add(bookshelf1)
   db.session.add(bookshelf2)
