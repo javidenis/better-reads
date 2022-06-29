024504f48efc7494cd13b8be545fb58131e7fa4d
I am Sheep <jorgexiaoyang@gmail.com>
2022-06-29 15:48:04 -0700

    ok

--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -2,12 +2,17 @@ import "./home.css";
 import logo from "../images/Screen Shot 2022-06-29 at 1.34.22 PM.png";
 import reading from "../images/file.jpeg";
 import LoginOptions from "../LoginOption/LoginOption";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 const Home = () => {
   const history = useHistory();
   const session = useSelector((state) => state.session);
+  const books = useSelector((state) => state.books);
+  const pics = Object.values(books).map((el) => (
+    <img alt="cover" src={el["cover_url"]} />
+  ));
+  console.log(pics);
 
   if (session.user) {
     history.push("/users");
@@ -24,12 +29,13 @@ const Home = () => {
       <div className="second_part">
         <img src={reading} alt="Logo" className="home_reading" />
       </div>
-      <div>
-        <LoginOptions />
+
+      <LoginOptions />
+
+      <div className="okay">
+        <div className="current_stuff">Our Current Colleciton!</div>
+        <div className="over">{pics}</div>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
     </div>
   );
 };
