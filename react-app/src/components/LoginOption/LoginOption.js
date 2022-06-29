412d0801c6cde18964c91c65015cc5cef3e95b86
I am Sheep <jorgexiaoyang@gmail.com>
2022-06-29 14:19:37 -0700

    some home

--- react-app/src/components/LoginOption/LoginOption.js
+++ react-app/src/components/LoginOption/LoginOption.js
@@ -5,7 +5,7 @@ const LoginOptions = () => {
     <div className="log_component">
       <div>
         <button className="login_button_log">
-          <NavLink to="/sign-up">Login</NavLink>
+          <NavLink to="/login">Login</NavLink>
         </button>
       </div>
       <div>
@@ -13,8 +13,12 @@ const LoginOptions = () => {
           <NavLink to="/sign-up">Sign Up</NavLink>
         </button>
       </div>
+      <div>
+        <div className="line"></div>
+      </div>
       <div className="demo_button">
         <span>Want to look around?</span>
+        <button>Demo User</button>
       </div>
     </div>
   );
