21cc5a24223a18bd351d248fdcfd204318bbb37d
I am Sheep <jorgexiaoyang@gmail.com>
2022-06-29 12:48:09 -0700

    navbar done css

--- react-app/src/components/NavBar/Logged.js
+++ react-app/src/components/NavBar/Logged.js
@@ -2,6 +2,7 @@ import { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
+import "./logged.css";
 
 const Logged = () => {
   //Checking login status
@@ -16,24 +17,27 @@ const Logged = () => {
   }, [session]);
 
   return (
-    <Fragment>
-      <div className="buttoninfo">
-        <div>{logged}</div>
-        <div>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </div>
-        <div>
-          <LogoutButton />
+    <div>
+      <div className="login_component">
+        <div className="login_info">
+          <div>{logged}</div>
+          <div>
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </div>
+
+          <div>
+            <NavLink to="/users" exact={true} activeClassName="active">
+              Users
+            </NavLink>
+          </div>
+          <div>
+            <LogoutButton />
+          </div>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
